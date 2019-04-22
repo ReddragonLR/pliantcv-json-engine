1cf8e03bbd53092ddc62e5281b28f6be41a50f67
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2019-04-22 19:25:13 +0200

    Initial commit of API and XUnit projects in dot net core

---
+++ src/pliantcv-json-engine.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace pliantcv_json_engine.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
